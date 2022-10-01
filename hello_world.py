45ae02af6080c18eabb0e6fff80ec25c2f0605ae
Pekka Nokelainen <pekka.nokelainen@tuni.fi>
2022-10-01 22:04:17 +0300

    Initial comment

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+"""
+COMP.CS.100
+Name: Pekka Nokelainen
+Email: pekka.nokelainen@tuni.fi
+Student Id: 150290744
+"""
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
